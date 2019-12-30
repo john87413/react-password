ebfafd87ce0e1e4a8b95c3a4a0c770bf0cc18493
john87413 <a0984009426@gmail.com>
2019-12-30 12:25:04 +0800

    Updates

---
+++ precache-manifest.8d24dd680085ae728ff46ec7d44c5ddd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0a898f214ac16e5a16de3e0a3383f74",
+    "url": "./index.html"
+  },
+  {
+    "revision": "19447496429ba1b6f39c",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e1ce69ecf5119febe4a1",
+    "url": "./static/js/2.2e98ccc5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "./static/js/2.2e98ccc5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "19447496429ba1b6f39c",
+    "url": "./static/js/main.137ee999.chunk.js"
+  },
+  {
+    "revision": "fb1f82c2d0b0ea233982",
+    "url": "./static/js/runtime-main.2b5aea54.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
